e7fb2d3fc5f4008eb671cdaf03a5dcb0ae3da032
Vic Chen <s179038@gmail.com>
2024-04-02 07:33:49 -0400

    add synthesis report of FFT_optimized

--- optimized/FFT/hls_code/FFT_optimized.c
+++ optimized/FFT/hls_code/FFT_optimized.c
@@ -22,13 +22,14 @@
 */
 #include "header.h"
 
-void FFT(fpr f[SIZE] /*, fpr f_out[SIZE]*/) {
+
+void FFT(fpr f[SIZE]) {
+//void FFT(stream_t *f_in, stream_t *f_out) {
 #pragma HLS INTERFACE m_axi port=f bundle=gmem0 offset=slave depth=1024
-//#pragma HLS INTERFACE axis port=f_in register_mode=both
-//#pragma HLS INTERFACE axis port=f_out register_mode=both
+//#pragma HLS INTERFACE axis port=f_in register both depth=1024
+//#pragma HLS INTERFACE axis port=f_out register both depth=1024
 #pragma HLS INTERFACE s_axilite port=return
 
-#pragma HLS DATAFLOW
 	// Create local buffer
 	fpr  in_buf_r[SIZE2],  in_buf_i[SIZE2];
 	fpr out_buf_r[SIZE2], out_buf_i[SIZE2];
@@ -46,14 +47,13 @@ void FFT(fpr f[SIZE] /*, fpr f_out[SIZE]*/) {
 }
 
 void fft_10_stages(fpr in_r[SIZE2], fpr in_i[SIZE2], fpr out_r[SIZE2], fpr out_i[SIZE2]) {
-	const unsigned short factor = FACTOR;
 	/* Array Partition */
 	fpr stage0_r[SIZE2], stage0_i[SIZE2];
 	fpr stage1_r[SIZE2], stage1_i[SIZE2];
-#pragma HLS ARRAY_PARTITION variable=stage0_r type=cyclic factor=factor
-#pragma HLS ARRAY_PARTITION variable=stage0_i type=cyclic factor=factor
-#pragma HLS ARRAY_PARTITION variable=stage1_r type=cyclic factor=factor
-#pragma HLS ARRAY_PARTITION variable=stage1_i type=cyclic factor=factor
+//#pragma HLS ARRAY_PARTITION variable=stage0_r type=block
+//#pragma HLS ARRAY_PARTITION variable=stage0_i type=block
+//#pragma HLS ARRAY_PARTITION variable=stage1_r type=block
+//#pragma HLS ARRAY_PARTITION variable=stage1_i type=block
 #pragma HLS INLINE off
 	fft_stage(    in_r,     in_i, stage0_r, stage0_i, 512, 2);
 	fft_stage(stage0_r, stage0_i, stage1_r, stage1_i, 256, 4);
@@ -75,10 +75,12 @@ void fft_stage(fpr in_r[SIZE2], fpr in_i[SIZE2], fpr out_r[SIZE2], fpr out_i[SIZ
 
 	fpr gm_re, gm_im;
 	fpr x_re, x_im, y_re, y_im, tmp_re, tmp_im;
+	fpr mul;
 
 	SECTION: for (int n = 0; n < 256; n++) {
-#pragma HLS UNROLL factor=4
-#pragma HLS PIPELINE II=4
+//#pragma HLS UNROLL factor=2
+#pragma HLS PIPELINE II=1
+//#pragma HLS INLINE
 		unsigned short i = n + ((n / ht) * ht);
 		unsigned short i_gm = n / ht;
 		gm_re = gm_re_tab[(m + i_gm)];
@@ -88,8 +90,7 @@ void fft_stage(fpr in_r[SIZE2], fpr in_i[SIZE2], fpr out_r[SIZE2], fpr out_i[SIZ
 		tmp_re = in_r[i + ht];
 		tmp_im = in_i[i + ht];
 		/* Mul */
-		y_re = (tmp_re * gm_re) - (tmp_im * gm_im);
-		y_im = (tmp_re * gm_im) + (tmp_im * gm_re);
+		complex_mul(&y_re, &y_im, tmp_re, tmp_im, gm_re, gm_im);
 		/* Add */
 		out_r[i] = x_re + y_re;
 		out_i[i] = x_im + y_im;
@@ -97,6 +98,29 @@ void fft_stage(fpr in_r[SIZE2], fpr in_i[SIZE2], fpr out_r[SIZE2], fpr out_i[SIZ
 		out_r[i + ht] = x_re - y_re;
 		out_i[i + ht] = x_im - y_im;
 	}
+}
+
+void complex_mul(fpr *z_re, fpr *z_im, fpr x_re, fpr x_im, fpr y_re, fpr y_im) {
+#pragma HLS INLINE off
+	//*z_re = (x_re * y_re) - (x_im * y_im);
+	//*z_im = (x_re * y_im) + (x_im * y_re);
+	// Rewrite as:
+	//*z_re = x_re * (y_re - y_im) + y_im * (x_re - x_im);
+	//*z_im = x_im * (y_re + y_im) + y_im * (x_re - x_im);
+
+	fpr sub_y = y_re - y_im;
+	fpr add_y = y_re + y_im;
+	fpr sub_x = x_re - x_im;
+	fpr mul1 = multiplier(x_re, sub_y);
+	fpr mul2 = multiplier(x_im, add_y);
+	fpr mul3 = multiplier(y_im, sub_x);
+	*z_re = mul1 + mul3;
+	*z_im = mul2 + mul3;
+
+}
+
+fpr multiplier(fpr a, fpr b) {
+	return (a * b);
 }
 
 
--- optimized/FFT/hls_code/tb.c
+++ optimized/FFT/hls_code/tb.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
@@ -11,19 +12,150 @@
 void test_FFT(void)
 {
 	fpr fft_out[1024];
+	int error_cnt = 0;
 	FFT(FFT_in);
     for(int i = 0; i<1024;++i)
-            if(FFT_in[i] != FFT_out[i])
-            printf("%d cal = %lf, cor = %lf \n", i, FFT_in[i], FFT_out[i]);
-        printf("\n \n");
+        if(round(FFT_in[i] * 1000000) != round(FFT_out[i] * 1000000)) {
+            printf("%d cal = %.17f, cor = %.17f \n", i, FFT_in[i], FFT_out[i]);
+            error_cnt += 1;
+        }
+    printf("\n \n");
+    if (error_cnt == 0) printf("Passed!!\n");
+    else				printf("Failed!!\n");
     printf("Test Finished \n");
 }
 
+void test_NTT(void) {
+	uint16_t *ntt_in1, *ntt_in2, *ntt_out1;
+	ntt_in1 = (uint16_t *)NTT_in;
+	uint16_t tmp[1024];
+	ntt_out1 = (uint16_t *) tmp;
+
+	int error_cnt = 0;
+
+	NTT(ntt_in1);
+
+	for (int j = 0; j < 1024; j++) {
+		if (ntt_in1[j] != NTT_out[j]) {
+			printf("Error at %d", j);
+			error_cnt += 1;
+		}
+	}
+	printf("\n\n");
+	if (error_cnt == 0) printf("Passed!!");
+	else				printf("Failed!!");
+	printf("\n\n");
+}
+
+
+/*
+void test_iFFT(void) {
+	iFFT(FFT_out, 10);
+	int fo, fi;
+	for (int i = 0; i < 1024; ++i) {
+		fo = round(FFT_out[i] * 100000);
+		fi = round(FFT_in[i] * 100000);
+		if (fo != fi) {
+			printf("%4d cal = %15f, cor = %15f \n", i, FFT_out[i], FFT_in[i]);
+		} else {
+			printf("%4d pass\n", i);
+		}
+	}
+	printf("Test Finished \n");
+}
+*/
+
+void test_index(int t, int m) {
+	int hm, ht;
+	hm = m >> 1;
+	ht = t >> 1;
+	int gm_or[256];
+	int gm_op[256];
+	int in_or[256];
+	int in_op[256];
+
+	int or_in = 0;
+	int op_in = 0;
+
+	int error_cnt = 0;
+	//printf("Original");
+
+	SECTION: for (int k = 0, i = 0; k < hm; k++, i += t) {
+	#pragma HLS UNROLL factor=4
+			int j, j2;
+			j2 = i + ht;
+			int gm_re, gm_im;
+			gm_re = ((m + k) << 1) + 0;
+			gm_im = ((m + k) << 1) + 1;
+			BUTTERFLY: for (j = i; j < j2; j++) {
+				//printf("gm %d, index %d \n", k, j);
+				gm_or[or_in] = gm_re;
+				in_or[or_in] = j;
+				or_in += 1;
+			}
+	}
+
+	for (int n = 0; n < 256; n++) {
+		int gm_re_new, gm_im_new;
+		unsigned short in = n + ((n / ht) * ht);
+		unsigned short in_gm = n / ht;
+		gm_re_new = ((m + in_gm) << 1) + 0;
+		gm_im_new = ((m + in_gm) << 1) + 1;
+		gm_op[n] = gm_re_new;
+		in_op[n] = in;
+		/*
+		if ((in + 1) % ht == 0) {
+			in += ht + 1;
+			in_gm += 1;
+		} else {
+			in += 1;
+		}
+		*/
+	}
+
+	for (int g = 0; g < 256; g++) {
+		if (gm_op[g] != gm_or[g]) {
+			printf("%d GM not equal \n", g);
+			error_cnt += 1;
+		}
+		if (in_op[g] != in_or[g]) {
+			printf("%d index not equal \n", g);
+			error_cnt += 1;
+		}
+	}
+
+	if (error_cnt == 0) printf("t = %d, m = %d passed\n", t, m);
+	else				printf("t = %d, m = %d failed\n", t, m);
+
+}
+
+void test_gm(void) {
+	//for (int i = 0; i < 1024; i++) {
+		//if (gm_re_tab[i] != fpr_gm_tab[(i << 1) + 0]) printf("%d not equal", i);
+		//if (gm_im_tab[i] != fpr_gm_tab[(i << 1) + 1]) printf("%d not equal", i);
+	//}
+}
 
 
 int
 main(void) {
 
-	test_FFT();
+	// test_python_h();
+	// test_FFT();
+	test_NTT();
+	// test_gm();
+	// test_iFFT();
+	// test_FFT_skeleton();
+/*
+	test_index(512, 2);
+	test_index(256, 4);
+	test_index(128, 8);
+	test_index(64, 16);
+	test_index(32, 32);
+	test_index(16, 64);
+	test_index(8, 128);
+	test_index(4, 256);
+	test_index(2, 512);
+*/
 	return 0;
 }
